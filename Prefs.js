38e73a3d38bec4303a01d89856fe7c4af78019a5
renderTom <tomas@rendertom.com>
2023-05-07 15:03:08 +0300

    refactor Prefs.js

--- Prefs.js
+++ Prefs.js
@@ -1,22 +1,46 @@
 var Prefs = (function () {
+  // @include "config/PREF_TO_FILTER.js"
+
   // @include "lib/ArrayEx.js"
   // @include "lib/FileEx.js"
   // @include "lib/FolderEx.js"
+  // @include "lib/ObjectEx.js"
   // @include "lib/Os.js"
   // @include "lib/StringEx.js"
 
   var module = {};
 
-  var MAP = {
-    PREF_Type_MACHINE_INDEPENDENT: '*-indep-general*',
-    PREF_Type_MACHINE_INDEPENDENT_COMPOSITION: '*-indep-composition*',
-    PREF_Type_MACHINE_INDEPENDENT_OUTPUT: '*-indep-output*',
-    PREF_Type_MACHINE_INDEPENDENT_RENDER: '*-indep-render*',
-    PREF_Type_MACHINE_SPECIFIC: 'Prefs*',
-    PREF_Type_MACHINE_SPECIFIC_PAINT: '*-paint*',
-    PREF_Type_MACHINE_SPECIFIC_TEXT: '*-text*',
+  /**
+   * @param {boolean} [isBeta]
+   * @returns {string}
+   */
+  module.getAppName = function (isBeta) {
+    isBeta = setValueIfUndefined(isBeta, app.isBeta);
+    return isBeta ? 'After Effects (Beta)' : 'After Effects';
   };
 
+  /**
+   * @param {string} [appVersion]
+   * @returns {string}
+   */
+  module.getAppVersion = function (appVersion) {
+    appVersion = setValueIfUndefined(appVersion, app.version);
+
+    var version = appVersion.split('x')[0];
+    if (version === '22.6.4') return version; // One time bug introduced by Adobe
+
+    var items = version.split('.');
+    var major = items[0];
+    var minor = items[1] || 0;
+    var patch = items[2] || 0;
+
+    return [major, minor].join('.');
+  };
+
+  /**
+   * @param {string} PREFType
+   * @returns {File}
+   */
   module.getFile = function (PREFType) {
     app.preferences.saveToDisk();
 
@@ -26,10 +50,17 @@ var Prefs = (function () {
     return files[0];
   };
 
+  /**
+   * @param {string} PREFType
+   * @returns {File[]}
+   * @throws {Error}
+   */
   module.getFiles = function (PREFType) {
     var folder = module.getFolder();
     if (!folder.exists) {
-      throw new Error('Preferences folder does not exist at path ' + folder.fsName);
+      throw new Error(
+        'Preferences folder does not exist at path ' + folder.fsName
+      );
     }
 
     var filter = getFilter(PREFType);
@@ -46,48 +77,63 @@ var Prefs = (function () {
     return files;
   };
 
-  module.getFolder = function () {
+  /**
+   * @param {boolean} [isWindows]
+   * @param {boolean} [isBeta]
+   * @param {string} [appVersion]
+   * @returns {Folder}
+   */
+  module.getFolder = function (isWindows, isBeta, appVersion) {
+    var rootFolder = module.getRootFolder(isWindows);
+    var appName = module.getAppName(isBeta);
+    var version = module.getAppVersion(appVersion);
+
     return FolderEx.getFolderObject(
-      getRootFolder() + '/Adobe/' + getAppName() + '/' + getAppVersion()
+      rootFolder.fsName + '/Adobe/' + appName + '/' + version
     );
   };
 
+  /**
+   * @param {string} PREFType
+   * @returns {Object}
+   */
   module.getJson = function (PREFType) {
     return toJson(FileEx.readFile(module.getFile(PREFType)));
+  };
+
+  /**
+   * @param {boolean} [isWindows]
+   * @returns {Folder}
+   */
+  module.getRootFolder = function (isWindows) {
+    isWindows = setValueIfUndefined(isWindows, Os.isWindows());
+
+    return FolderEx.getFolderObject(
+      isWindows
+        ? Folder.userData.fsName
+        : Folder.userData.parent.fsName + '/Preferences'
+    );
+  };
+
+  /**
+   * @returns {string[]}
+   */
+  module.getSupportedPrefTypes = function () {
+    return ObjectEx.keys(PREF_TO_FILTER);
   };
 
   return module;
 
   ///
 
-  function getAppName() {
-    return app.isBeta ? 'After Effects (Beta)' : 'After Effects';
-  }
-
-  function getAppVersion() {
-    var version = app.version.split('x')[0];
-    if (version === '22.6.4') return version; // One time bug introduced by Adobe
-
-    var items = version.split('.');
-    var major = items[0];
-    var minor = items[1] || 0;
-    var patch = items[2] || 0;
-
-    return [major, minor].join('.');
-  }
-
   function getFilter(PREFType) {
     PREFType = PREFType || 'PREF_Type_MACHINE_SPECIFIC';
 
-    if (!MAP.hasOwnProperty(PREFType)) {
+    if (!PREF_TO_FILTER.hasOwnProperty(PREFType)) {
       throw new Error('PREFType ' + PREFType + ' does not exist');
     }
 
-    return MAP[PREFType];
-  }
-
-  function getRootFolder() {
-    return Os.isWindows() ? Folder.userData.fsName : Folder.userData.parent.fsName + '/Preferences';
+    return PREF_TO_FILTER[PREFType];
   }
 
   function isComment(string) {
@@ -100,6 +146,10 @@ var Prefs = (function () {
 
   function joinMultiline(string) {
     return string.replace(/"*\\[\n|\r]\s*"?/g, '');
+  }
+
+  function setValueIfUndefined(value, fallbackValue) {
+    return typeof value === 'undefined' ? fallbackValue : value;
   }
 
   function sortByDate(f1, f2) {
---
+++ config/PREF_TO_FILTER.js
@@ -0,0 +1,12 @@
+var PREF_TO_FILTER = {
+  PREF_Type_MACHINE_INDEPENDENT: '*-indep-general*',
+  PREF_Type_MACHINE_INDEPENDENT_COMPOSITION: '*-indep-composition*',
+  PREF_Type_MACHINE_INDEPENDENT_OUTPUT: '*-indep-output*',
+  PREF_Type_MACHINE_INDEPENDENT_RENDER: '*-indep-render*',
+  
+  // TODO: create a working filter
+  // PREF_Type_MACHINE_SPECIFIC: 'Prefs*',
+
+  PREF_Type_MACHINE_SPECIFIC_PAINT: '*-paint*',
+  PREF_Type_MACHINE_SPECIFIC_TEXT: '*-text*',
+};
